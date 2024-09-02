26b0fafbe2973721529638604edb06c01e15bdec
Jonatandb <jonatandb@gmail.com>
2024-09-01 22:58:18 -0300

    Github pages vite config

--- 03-social-media-dashboard-with-theme-switcher-master/dashboard/vite.config.js
+++ 03-social-media-dashboard-with-theme-switcher-master/dashboard/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FrontendMentor-Challenges/"
+  base: "/FrontendMentor-Challenges/03-social-media-dashboard-with-theme-switcher-master/dashboard/dist/"
 })
